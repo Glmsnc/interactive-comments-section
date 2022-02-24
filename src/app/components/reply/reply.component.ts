8ccf7bef4d908951fb37322eff226b2bc538834a
Guilherme Maciel <guilherme.mpereira@ambevtech.com.br>
2022-02-24 13:28:14 -0300

    added inputs to reply

--- src/app/components/reply/reply.component.ts
+++ src/app/components/reply/reply.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import UserService from '../services/user.service';
 
 @Component({
   selector: 'app-reply',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReplyComponent implements OnInit {
 
-  constructor() { }
+  textContent: any;
+  @Input() replyTo = '';
+  @Input() isNewMessage = false;
+  @Output() close = new EventEmitter();
+  @Output() sendMessage = new EventEmitter();
+  constructor(readonly userService: UserService) { }
 
   ngOnInit(): void {
+    if(this.replyTo) this.textContent = `@${this.replyTo},`;
   }
 
+  send(){
+    let replyingTo = '' 
+    this.textContent = this.textContent.split('@')[1];
+    if(this.textContent.includes(',')){
+      replyingTo = this.textContent.split(',')[0];
+      this.textContent = this.textContent.split(',')[1];
+    }
+    else{
+      replyingTo = this.textContent.split(' ')[0];
+      this.textContent = this.textContent.split(' ')[1];
+    }
+    this.sendMessage.emit({replyingTo, content:this.textContent});
+
+  }
+
+  changeText(){
+    if(!this.textContent.includes('@')) this.close.emit();
+  }
 }
