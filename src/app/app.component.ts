6437b1bb325d2e5e45529f6c57a19d29585d54ab
Guilherme Maciel <guilherme.mpereira@ambevtech.com.br>
2022-02-24 13:28:48 -0300

    change app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import CommentService from './components/services/comment.service';
+import UserService from './components/services/user.service';
+import * as data from './data.json';
+import { CommentData } from './models/Comment';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'interactive-comments-section-main';
+export class AppComponent implements OnInit{
+  currentUser: User = null;
+  comments: CommentData[] = [];
+  constructor(
+    private readonly userService: UserService,
+    private readonly commentService: CommentService){
+    this.loadData();
+    this.saveUser();
+  }
+
+  ngOnInit(): void {
+    this.commentService.comment$.subscribe
+    (comments => {
+      this.comments = comments;
+      console.log('teste', this.comments)
+    });
+  }
+
+  loadData(){
+    const {comments, currentUser} = data;
+    this.currentUser = currentUser;
+    this.saveUser();
+    this.saveComments(comments);
+
+  }
+
+  saveUser(){
+    this.userService.saveUser(this.currentUser);
+  }
+  saveComments(comments: any){
+    this.commentService.saveComments(comments);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommentsComponent } from './components/comments/comments.component';
+import { ButtonReply } from './components/shared/buttons/reply/button-reply.component';
+import { ButtonPlus } from './components/shared/buttons/plus/button-plus.component';
+import { ButtonMinus } from './components/shared/buttons/minus/button-minus.component';
 import { ReplyComponent } from './components/reply/reply.component';
+import { ReplyListComponent } from './components/reply-list/reply-list.component';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DialogDeleteComponent } from './components/shared/dialog-delete/dialog-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CommentsComponent,
-    ReplyComponent
+    ReplyComponent,
+    ButtonPlus,
+    ButtonMinus,
+    ButtonReply,
+    ReplyListComponent,
+    CommentListComponent,
+    DialogDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
