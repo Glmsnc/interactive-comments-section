aecd5eb1b4960524301d6c62e3ed808830446dda
Guilherme Maciel <guilherme.mpereira@ambevtech.com.br>
2022-02-24 13:27:42 -0300

    added a component for list of comment

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,15 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommentData } from 'src/app/models/Comment';
+import CommentService from '../services/comment.service';
+import UserService from '../services/user.service';
 
 @Component({
-  selector: 'app-comments',
+  selector: 'comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.less']
 })
 export class CommentsComponent implements OnInit {
 
-  constructor() { }
+  @Input() isReply = false;
+  @Input() comment: CommentData;
+  constructor(readonly userService: UserService,
+    readonly commentService: CommentService) { }
+
+  openModal: boolean = false;
+  editComment = false;
+  openReply = false;
+  isCurrentUser = false;
 
   ngOnInit(): void {
+    this.isCurrentUser = (this.userService.user.username === this.comment.user.username);
   }
 
+
+  addReply(newReply: any){
+    const lastId = this.getLastId();
+    const reply = {
+      id: lastId,
+      content: newReply.content,
+      createdAt: 'Today',
+      score: 0,
+      replyingTo: newReply.replyingTo ,
+      user: this.userService.user
+    }
+    if(!this.isReply){
+      this.comment.replies.push(reply)
+    }
+    else{
+      this.commentService.comment.map(
+          comment =>{
+              if (comment.id == this.getParentId()) comment.replies.push(reply);
+          }
+      );
+    }
+
+    this.commentService.saveComments(this.commentService.comment);
+    this.openReply = false;
+  }
+
+  getLastId(): number{
+    let lastId = 0;
+    console.log(this.commentService.comment)
+    this.commentService.comment.map(comment =>{
+      if(comment.id > lastId) lastId = comment.id;
+      comment.replies.map(reply =>{ if(reply.id > lastId) lastId = reply.id});
+      return comment;
+    })
+    return lastId+1;
+  }
+
+
+  getParentId(): number{
+    let parentId: any;
+    console.log('commentado',this.commentService.comment)
+
+    parentId = this.commentService.comment.find(item =>{
+      let test = item.replies.filter(item => item.id == this.comment.id)
+     
+      return test.length;
+      })?.id;
+      console.log(parentId);
+    return parentId;
+  }
+
+  downVote(){
+    this.comment.score--;
+  }
+
+  upVote(){
+    this.comment.score++;
+  }
+
+  toggleReply(){
+    this.openReply = !this.openReply;
+  }
+  toggleModal(){
+    this.openModal = !this.openModal;
+  }
+
+  delete(){
+    this.commentService.removeComment(this.comment.id);
+    this.openModal = false;
+  }
+
+  toggleEdit(){
+    this.editComment = !this.editComment;
+  }
+
+  edit(){
+    this.editComment = true;
+  }
 }
